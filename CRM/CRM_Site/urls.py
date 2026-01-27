f7d3bb5d497cccb5b248f9c63b465e49355e022f
Pedro Thomas <124922008+Prthomas808@users.noreply.github.com>
2026-01-27 14:52:17 -0500

    delete record function added

--- CRM/CRM_Site/urls.py
+++ CRM/CRM_Site/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("login/", views.login_user, name="login"),
     path("logout/", views.logout_user, name="logout"),
+    path("record/<int:pk>", views.customer_record, name="record"),
+    path("delete_customer/<int:pk>", views.delete_customer, name="delete_customer"),
+    path("update_customer/<int:pk>", views.update_customer, name="update_customer"),
 ]
--- CRM/CRM_Site/views.py
+++ CRM/CRM_Site/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .models import Record
@@ -13,3 +13,19 @@ def login_user(request):
 
 def logout_user(request):
     pass
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        record = Record.objects.get(id=pk)
+        return render(request, "customer_record.html", {"record" : record})
+    
+def delete_customer(request, pk):
+    if request.user.is_authenticated:
+        customer = Record.objects.get(id=pk)
+        customer.delete()
+        messages.success(request, "{customer.first_name} deleted")
+        return redirect("home")
+    else:
+        messages.success(request, "Only admin can access this")
+        return redirect("home")
+    
